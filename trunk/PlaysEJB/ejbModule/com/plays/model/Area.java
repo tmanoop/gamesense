c5d7ed3a2e83f9c54101569532ad185d2ecdbd6f
mt57@njit.edu <mt57@njit.edu@f8fcaa78-9446-0437-eb42-da0a66d47071>
2013-10-29 17:46:08 +0000

    added tile columns

--- trunk/PlaysEJB/ejbModule/com/plays/model/Area.java
+++ trunk/PlaysEJB/ejbModule/com/plays/model/Area.java
@@ -18,20 +18,23 @@ public class Area implements Serializable {
 	@Column(name="SQAURE_ID")
 	private int sqaureId;
 
+	@Column(name="COVERED_IND")
+	private String coveredInd;
+
 	@Column(name="FLOOR_NUM")
 	private int floorNum;
-
-	@Column(name="GPS_LAT")
-	private double gpsLat;
-
-	@Column(name="GPS_LNG")
-	private double gpsLng;
 
 	@Column(name="ROOM_NUM")
 	private String roomNum;
 
 	@Column(name="SQUARE_DESC")
 	private String squareDesc;
+
+	@Column(name="TILE_X")
+	private double tileX;
+
+	@Column(name="TILE_Y")
+	private double tileY;
 
 	public Area() {
 	}
@@ -44,28 +47,20 @@ public class Area implements Serializable {
 		this.sqaureId = sqaureId;
 	}
 
+	public String getCoveredInd() {
+		return this.coveredInd;
+	}
+
+	public void setCoveredInd(String coveredInd) {
+		this.coveredInd = coveredInd;
+	}
+
 	public int getFloorNum() {
 		return this.floorNum;
 	}
 
 	public void setFloorNum(int floorNum) {
 		this.floorNum = floorNum;
-	}
-
-	public double getGpsLat() {
-		return this.gpsLat;
-	}
-
-	public void setGpsLat(double gpsLat) {
-		this.gpsLat = gpsLat;
-	}
-
-	public double getGpsLng() {
-		return this.gpsLng;
-	}
-
-	public void setGpsLng(double gpsLng) {
-		this.gpsLng = gpsLng;
 	}
 
 	public String getRoomNum() {
@@ -82,6 +77,22 @@ public class Area implements Serializable {
 
 	public void setSquareDesc(String squareDesc) {
 		this.squareDesc = squareDesc;
+	}
+
+	public double getTileX() {
+		return this.tileX;
+	}
+
+	public void setTileX(double tileX) {
+		this.tileX = tileX;
+	}
+
+	public double getTileY() {
+		return this.tileY;
+	}
+
+	public void setTileY(double tileY) {
+		this.tileY = tileY;
 	}
 
 }
